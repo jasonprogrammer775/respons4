104f9642d00ed48981b72be296adb5aec8d9d47f
Jay <74604447+jasonprogrammer775@users.noreply.github.com>
2025-03-15 13:38:22 +0100

    ✨

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,31 @@ import ActionsCard from './components/ActionsCard'
 import TrafficCard from './components/TrafficCard'
 import ResourcesCard from './components/ResourcesCard'
 import { Grid } from './styles/StyledComponents'
+import styled from '@emotion/styled'
+
+const IframeContainer = styled.div`
+  width: 100%;
+  height: 400px;
+  margin-bottom: 1rem;
+  border-radius: 16px;
+  overflow: hidden;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  
+  iframe {
+    width: 100%;
+    height: 100%;
+    border: none;
+  }
+`
 
 function App() {
   return (
-    <Grid>
-      <WeatherCard />
+    <>
+      <IframeContainer>
+        <iframe src="https://avatarjay.netlify.app/" title="Avatar Jay" />
+      </IframeContainer>
+      <Grid>
+        <WeatherCard />
       <NewsCard />
       <MarsWeatherCard />
       <CryptoCard />
@@ -21,6 +41,7 @@ function App() {
       <TrafficCard />
       <ResourcesCard />
     </Grid>
+    </>
   )
 }
 
